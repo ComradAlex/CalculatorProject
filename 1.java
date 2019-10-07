d1e5a6b5baabb003f71184b2f08c5462d4515ffe
Sigarev Alexander <komrad.alex3002@yandex.ru>
2019-10-07 14:01:23 +0300

    Added calculator

---
+++ 1.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int number1 = GettingSymbols.getInt();
+        int number2 = GettingSymbols.getInt();
+        char operation = GettingSymbols.getOperation();
+        int result = Calculations.calculate(number1,number2,operation);
+        System.out.println("Результат операции: "+result);
+    }
+
+    private static class GettingSymbols {
+
+        public static int getInt() {
+            System.out.println("Введите число:");
+            int num;
+            if (scanner.hasNextInt()) {
+                num = scanner.nextInt();
+            } else {
+                System.out.println("Неверно введено число. Повторите попытку.");
+                scanner.next();
+                num = getInt();
+            }
+            return num;
+        }
+
+        public static char getOperation() {
+            System.out.println("Введите операцию:");
+            char operation;
+            if (scanner.hasNext()) {
+                operation = scanner.next().charAt(0);
+            } else {
+                System.out.println("Неверно введена операция. Повторите попытку.");
+                scanner.next();
+                operation = getOperation();
+            }
+            return operation;
+        }
+    }
+
+    public static class Calculations {
+
+        public static int calculate(int number1, int number2, char operation) {
+            int result;
+            switch (operation) {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    result = number1 / number2;
+                    break;
+                default:
+                    System.out.println("Операция не распознана. Повторите ввод.");
+                    result = calculate(number1, number2, GettingSymbols.getOperation());
+            }
+            return result;
+        }
+    }
+}
+
